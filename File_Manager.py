392091ed3a1e19c2eb39f0d97f5ffc180d30cd3b
Bruno Campelo <bruno5rafael@hotmail.com>
2025-04-10 20:53:20 -0300

    feat: Finish the File Manager implementation

--- File_Manager.py
+++ File_Manager.py
@@ -1,88 +1,81 @@
 import os
 import shutil
-import basic_file_path
-from implant_logging import logger
+from logger import Logger
 
 class FileManager:
-    def __init__(self):
-        pass
 
-    def __is_cancel_command(self, command):
-        '''Método que verifica se o comando é de cancelamento'''
-        command = command.strip().lower()
-        return command in ["cancelar", "-c"]
+    def list_files(self, origin_path):
+        return os.listdir(origin_path)
 
-    def __basic_file_path(self, origin_path, destination_path):
-        
-        
-        # O código abaixo verifica se a pasta de origem existe
-        if not os.path.exists(origin_path):
-            print(f'Erro: O diretório "{origin_path}" não existe.')
-            return
-        
-        # Verifica se o destino é diferente da origem
+    def move_files_by_type(self, origin_path, destination_path, extensions_list):
+        logger = Logger("FileManager")
+        # Verifica se a pessoa colocou os paths das pastas
+        if not origin_path or not destination_path:
+            print("Erro: O caminho de origem ou destino está vazio!")
+
+        # Verifica se a origem e o destino são a mesma coisa
         if os.path.abspath(origin_path) == os.path.abspath(destination_path):
-            print('Erro: O diretório de destino não pode ser o mesmo que o de origem.')
+            print("Erro: A pasta de destino não pode ser a mesma da origem.")
+
+        # Lista todos os arquivos da origem
+        files = self.list_files(origin_path)
+
+        if not files:
+            print("A pasta de origem está vazia!")
             return
-        # Se a pasta de destino não existir ela é criada automaticamente
-        os.makedirs(destination_path, exist_ok=True)   
 
-    def move_all_files(self, origin_path, destination_path):
-        if self._check_paths():
-            files = os.listdir(origin_path)
-            if not files:
-                print('Erro ao mover: O diretório de origem está vazio!')
-                logger.info(f'Erro ao mover: o diretório de origem está vazio: {origin_path}')
-                return
+        for file in files:
+            path_file = os.path.join(origin_path, file)
 
-            for file in files:
-                self.move_file(file, origin_path, destination_path)
+            # Verifica se é um arquivo
+            if os.path.isfile(path_file):
+                # Pega a extensão do arquivo
+                for ext in extensions_list:
+                    if file.endswith(ext):
+                        os.makedirs(destination_path, exist_ok=True)
 
-    def move_file(self, file, origin_path, destination_path):
-        self.__basic_file_path(origin_path, destination_path)
-        try:
-            source = os.path.join(origin_path, file)
-            destination = os.path.join(destination_path, file)
-
-            if os.path.isfile(source):
-                shutil.move(source, destination)
-                print(f'Arquivo {file} movido com sucesso.')
-                logger.info(f'Arquivo movido com sucesso: {file}')
+                        # Move o arquivo
+                        try:
+                            shutil.move(path_file, os.path.join(destination_path, file))
+                            logger.info(f'Arquivo "{file}" movido para a pasta "{destination_path}".')
+                        except Exception as erro:
+                            logger.error(f'Erro ao mover o file "{file}": {erro}')
+                        break
             else:
-                print(f'Erro: {file} não é um arquivo, pulando...')
-                logger.info(f'Erro: não se trata de um arquivo: {file}')
-        except Exception as e:
-            print(f'Erro ao mover o arquivo "{file}": {str(e)}')
-            logger.info(f'Erro ao mover o arquivo "{file}": {str(e)}')
-
-    def _check_paths(self):
-        if origin_path is None or destination_path is None:
-            return False
-        return True
-
-    def list_files_in_origin_path(self, origin_path):
-        return [f for f in os.listdir(origin_path) if os.path.isfile(f)]
+                print(f'"{file}" não é um arquivo, ignorando.')
     pass
 
 if __name__ == '__main__':
     file_manager = FileManager()
+
+    types = {
+        'Imagens': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp'],
+        'Documentos': ['.pdf', '.txt', '.docx', '.doc', '.xls', '.xlsx', '.ppt', '.pptx', '.odt'],
+        'Musicas': ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.m4a'],
+        'Vídeos': ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm'],
+        }
+
     while True:
         print("\nEscolha uma opção:")
-        print("1. Mover arquivos")
-        print("2. Listar todos os arquivos.")
+        print("1. Mover arquivos por tipo")
+        print("2. Listar todos os arquivos")
         print("3. Sair do programa")
-        
+
         choice = input("Digite sua escolha (1 a 3): ")
-        origin_path = str(input("Pasta de origem: "))
+        origin_path = input("Pasta de origem: ")
+
         if choice == "1":
-            destination_path = str(input("Pasta de destino: "))
-            file_manager.move_all_files(origin_path, destination_path)
+            destination_path = input("Pasta de destino: ")
+            file_manager.move_files_by_type(origin_path, destination_path, types["Imagens"])
+
         elif choice == "2":
-            files = file_manager.list_files_in_origin_path(origin_path)
+            files = file_manager.list_files(origin_path)
             print(files)
+
         elif choice == "3":
             print("Programa encerrado.")
             break
+
         else:
             print("Opção inválida!")
             
